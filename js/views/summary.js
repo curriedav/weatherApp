49a79cc2dc09718b850cd80e6660f7e77358623c
curriedav <curriedav@gmail.com>
2013-12-22 23:36:34 -0800

    repairs bugs

--- js/views/summary.js
+++ js/views/summary.js
@@ -10,9 +10,28 @@ var SummaryView = Backbone.View.extend({
 
   render: function () {
       if (this.model.get('currently')) {
-        this.$el.html(this.template(this.model.get('currently')));
-      }
+        var context = this.buildContext();
+        this.$el.html(this.template(context));
+      };
     return this;
+  },
+  buildContext: function () {
+    var curr = this.model.get('currently');      
+    var context = {
+      summary: curr.summary,
+      apparentTemperature: Math.floor(curr.apparentTemperature),
+      dewPoint: Math.floor(curr.dewPoint),
+      humidity: curr.humidity * 100,
+      ozone: Math.floor(curr.ozone),
+      precipType: this.model.get('daily').data[0].precipType,
+      precipProbability: curr.precipProbability * 100,
+      temperature: Math.floor(curr.temperature),
+      visibility: Math.floor(curr.visibility),
+      windSpeed: Math.floor(curr.windSpeed),
+      alerts: this.model.get("alerts") || {}
+    };
+
+    return context;
   }
 });
 
